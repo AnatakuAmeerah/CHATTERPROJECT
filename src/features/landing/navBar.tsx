241e7f91d169bc7e3cc611fdb50866f7234906fa
AnatakuAmeerah <anatakuameerah@gmail.com>
2024-03-19 06:13:57 -0700

    responsive-editing

--- src/features/landing/navBar.tsx
+++ src/features/landing/navBar.tsx
@@ -12,6 +12,7 @@ function Navbar() {
         e.preventDefault()
         const target = e.target.getAttribute('href')
         const location = document.querySelector(target).offsetTop
+        
 
         window.scrollTo({
             left:0,
